210f39f1b0b05dba3853169a70bf4d142e5f35f2
GroovyCat <kodeep11@gmail.com>
2019-04-03 20:15:15 +0900

    Update search_list function
    GroovyCat - 2015244006 채윤재

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 	리스트 데이터 삭제 : 박문영 - parkmy123
 	리스트 데이터 삽입 : 지하린 - JiHaRin
-	리스트 데이터 검색 : 채윤재 - GroovyCat
+	리스트 데이터 검색 : 채윤재 - GroovyCat - 수정 : 함수 선언, 정의 순서 변경, malloc.h 선언, 리스트 검색 함수
 	리스트 데이터 출력 : 우성 - woo sung
 
 	뼈다구만 만든거니 각자 맡은 부분 원하시는 데로 수정하시면 됩니다. 
@@ -11,7 +11,7 @@
 */
 
 #include <stdio.h>
-
+#include <malloc.h>  // GroovyCat 수정
 // 노드 구조체 선언
 typedef struct cell {
 	int element;
@@ -20,35 +20,11 @@ typedef struct cell {
 
 node *head = NULL;
 
-//앞에 데이터 삽입
-void insert_head_of_list(int val)
-{
-	printf("HAHA\n");
-}
-
-//뒤에 데이터 삽입
-void insert_end_of_list(int val)
-{
-	printf("HAHA\n");
-}
-
-//연결리스트 출력
-void print_list()
-{
-	printf("HAHA\n");
-}
-
-//리스트 데이터 삭제
-void del_list()
-{
-	printf("HAHA\n");
-}
-
-//리스트 데이터 검색
-void search_list()
-{
-	printf("HAHA\n");
-}
+void insert_head_of_list(int val);  
+void insert_end_of_list(int val);   
+void print_list();                  
+void del_list();                    
+void search_list(node* head, int val); // 검색하고자 하는 데이터를 입력해 해당 데이터가 몇번째 노드에 있는지 확인하는 함수 
 
 int main(void)
 {
@@ -76,8 +52,11 @@ int main(void)
 			insert_end_of_list(num);
 		else if (check == 3)
 			del_list();
-		else if (check == 4)
-			search_list();
+		else if (check == 4) {
+			printf("검색하고자 하는 값을 입력하세요 : ");
+			scanf_s("%d", &num);
+			search_list(head, num);
+		}
 		else if (check == 5)
 			print_list();
 		else if (check == 6)
@@ -85,4 +64,43 @@ int main(void)
 	}
 
 	return 0;
+}
+
+//앞에 데이터 삽입
+void insert_head_of_list(int val)
+{
+	printf("HAHA\n");
+}
+
+//뒤에 데이터 삽입
+void insert_end_of_list(int val)
+{
+	printf("HAHA\n");
+}
+
+//연결리스트 출력
+void print_list()
+{
+	printf("HAHA\n");
+}
+
+//리스트 데이터 삭제
+void del_list()
+{
+	printf("HAHA\n");
+}
+
+//리스트 데이터 검색
+void search_list(node* head, int val)
+{
+ 	node* cur = head; // 앞 노드
+	int n = 1;  // ?번째 노드를 나타내는 변수
+	while (cur != NULL) {
+		node* tmp = cur->next; 
+		if (val == cur->element) {
+			printf("%d 번째 노드에 있습니다.\n", n);
+		}	
+		n++;
+		cur = tmp;
+	}
 }
